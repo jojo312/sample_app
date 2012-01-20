3d3e0108d4cec832d44d7be5325a03ce84aa0bf4
= <=>
2012-01-20 11:36:01 -0600

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
+  resources :users
   root :to => 'pages#home'
-  get "users/new"
   match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
